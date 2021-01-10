6293a082ac4df4ba4f8923622a50a85bc8311c70
William R. Fraser <wfraser@codewise.org>
2021-01-10 15:28:57 -0800

    move tests to a testlib

---
+++ tests/testlib.rs
@@ -0,0 +1,28 @@
+use error_remark::ResultExt;
+
+#[test]
+fn stacking_messages() {
+    let first = Result::<(), _>::Err("first inner").err_remark("first remark").unwrap_err();
+    assert_eq!(first.to_string(), "first remark: first inner");
+
+    let second = Result::<(), _>::Err(first).err_remark("second remark").unwrap_err();
+    assert_eq!(second.to_string(), "second remark: first remark: first inner");
+}
+
+#[test]
+fn context_values() {
+    let err = Result::<(), _>::Err("inner error")
+        .err_remark_vars("context msg", &[&"static str", &77, &String::from("owned string too")])
+        .unwrap_err();
+    assert_eq!(
+        err.to_string(),
+        "context msg (\"static str\", 77, \"owned string too\"): inner error"
+    );
+}
+
+#[test]
+fn deref_equality() {
+    let result = Result::<(), i32>::Err(777).err_remark("hey there").unwrap_err();
+    // Remark doesn't implement PartialEq or Eq, but you can deref it to get the inner.
+    assert_eq!(*result, 777);
+}
